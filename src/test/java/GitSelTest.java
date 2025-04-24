3022e71213b5b1e7ac5166419047437412360031
Marie Ch <zima04@mail.ru>
2025-04-24 19:38:23 +0300

    тест ДЗ 4 урок

---
+++ src/test/java/GitSelTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GitSelTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void checkAssertPage() {
+        // Открыть страницу репозитория Selenide
+        open("selenide/selenide");
+        // Перейти в раздел Wiki проекта
+        $(("#wiki-tab")).click();
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-content").shouldHave(text("Soft assertions"));
+        $("#wiki-content").$(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
+
